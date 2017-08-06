e31f78ada074f6454e3672abffa0e2847c3bbb7d
Dylan Reuter <djreuter45@gmail.com>
2017-08-06 09:08:03 -0600

    created necessary files and boiler plate code

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8"/>
+		<title>JS Assignment</title>
+		<link href="style.css" rel="stylesheet"/>
+		<script type="text/javascript" src="script.js"></script>
+	</head>
+	<body>
+
+	</body>
+</html>
